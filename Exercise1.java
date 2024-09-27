e73a7b0761f3a2cc01acbd6c720a0bf6ffff88a1
ywlee <lyw2037@naver.com>
2024-09-27 14:43:19 +0900

    First Commit

---
+++ Exercise1.java
@@ -0,0 +1,17 @@
+package day1;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		
+		int a = 10;
+		int b = 25;
+		int c = 33;
+		int d = a+b+c;
+		
+		System.out.println("합계 : "+(d));
+		System.out.println("평균 : "+(d)/3);
+
+	}
+
+}
